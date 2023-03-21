c3a774bcaec06ff0309b040a52ae1314a7674305
Sakshi Rai <sakshirai996@gmail.com>
2023-03-21 14:37:29 +0530

    change title name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,7 +22,7 @@ function Header() {
   return (
     <AppBar sx={{ backgroundColor: "#1e8185" }} position="sticky">
       <Toolbar>
-        <Typography variant="h5">UPICON App</Typography>
+        <Typography variant="h5">Vlog App</Typography>
         {isLoggedIn && (
           <Box display="flex" marginLeft={"auto"} marginRight={"auto"}>
             <Tabs
@@ -30,9 +30,9 @@ function Header() {
               value={value}
               onChange={(value) => setValue(value)}
             >
-              <Tab LinkComponent={Link} to="/blogs" label="All Data" />
-              <Tab LinkComponent={Link} to="/myBlogs" label="My Data" />
-              <Tab LinkComponent={Link} to="/blogs/add" label="Add Data" />
+              <Tab LinkComponent={Link} to="/blogs" label="All Vlog" />
+              <Tab LinkComponent={Link} to="/myBlogs" label="My Vlog" />
+              <Tab LinkComponent={Link} to="/blogs/add" label="Add Vlog" />
             </Tabs>
           </Box>
         )}
